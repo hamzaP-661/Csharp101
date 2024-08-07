32e2cd74a2128f611640e8a9c0eedd1bc9e1c942
hamzaP-661 <limitless6116@gmail.com>
2024-08-07 20:29:18 +0300

    ForLoop

---
+++ ForLoop/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ForLoop;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //Ekrandan girilen sayıya kadar olan tek sayıları yazdır.
+        /*Console.WriteLine("Bir sayi giriniz: ");
+        int sayac = int.Parse(Console.ReadLine());
+
+        for (int i = 1; i <= sayac; i++)
+        {
+            if (i % 2 == 1)
+            {
+                Console.WriteLine(i);
+            }
+        }*/
+
+        //1 ile 1000 arasındaki tek ve çift sayıların kendi içlerindeki toplamlarını ekrana yazdır.
+        int tekToplam = 0;
+        int ciftToplam = 0;
+
+        for (int i = 1; i <= 1000; i++)
+        {
+            if (i % 2 == 1)
+                tekToplam += i;
+            else
+                ciftToplam += i;
+        }
+
+        Console.WriteLine("Tek Toplam = " + tekToplam + " Çift Toplam = " + ciftToplam);
+
+        //break, continue
+
+        for (int i = 1; i < 10; i++)
+        {
+            if (i == 4)
+                break;
+
+            Console.WriteLine(i);
+        }
+    }
+}
